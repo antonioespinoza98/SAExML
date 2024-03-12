352b0fb6df87668ce7da55e9f84b71cb3ab80d09
antonioespinoza98 <espinozamarco70@gmail.com>
2024-03-12 18:01:37 -0300

    archivos y folders iniciales

---
+++ python/BosquesA.py
@@ -0,0 +1,39 @@
+from sklearn.model_selection import train_test_split
+from sklearn import preprocessing, utils, tree
+import pyreadr
+import pandas as pd
+import graphviz
+import matplotlib.pyplot as plt
+
+# BASE DE DATOS
+## Como la base está en un archivo RData, se utiliza el paquete de pyreadr para poder leerlo en Python.
+
+result = pyreadr.read_r("./data/ingreso/encuesta_df_agg.Rdata")
+print(result.keys())
+base = result["encuesta_df_agg"]
+## Ahora se crea la base de datos de entrenamiento y de prueba
+## Revisamos que la base de datos esté completa
+base.head()
+list(base.columns)
+
+X = base.drop(base.columns[6], axis=1)
+y = base.iloc[:, 6]
+X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=0.8, random_state= 25)
+lab_enc = preprocessing.LabelEncoder()
+y_train = lab_enc.fit_transform(y_train)
+print(utils.multiclass.type_of_target(y_train))
+
+
+clf = tree.DecisionTreeClassifier(max_depth= 5)
+clf = clf.fit(X_train, y_train)
+
+
+tree.plot_tree(clf)
+plt.show()
+
+dot_data = tree.export_graphviz(clf, out_file=None,
+                                filled=True, rounded=True,  
+                                special_characters=True)  
+graph = graphviz.Source(dot_data)  
+graph
+plt.show()

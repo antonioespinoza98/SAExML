13975b279710c0a4a1571cd1422759099ed40b1a
antonioespinoza98 <espinozamarco70@gmail.com>
2024-03-13 09:20:28 -0300

    Cambios

--- python/ingreso/BosquesA.py
+++ python/ingreso/BosquesA.py
@@ -1,3 +1,7 @@
+############################################################
+############  LIBRERIAS
+############
+############################################################
 from sklearn.model_selection import train_test_split
 from sklearn import preprocessing, utils, tree
 import pyreadr
@@ -5,6 +9,10 @@ import pandas as pd
 import graphviz
 import matplotlib.pyplot as plt
 
+############################################################
+############  BASE DE DATOS
+############
+############################################################
 # BASE DE DATOS
 ## Como la base está en un archivo RData, se utiliza el paquete de pyreadr para poder leerlo en Python.
 
@@ -16,6 +24,10 @@ base = result["encuesta_df_agg"]
 base.head()
 list(base.columns)
 
+############################################################
+############  ENTRENAMIENTO/PRUEBA
+############
+############################################################
 X = base.drop(base.columns[6], axis=1)
 y = base.iloc[:, 6]
 X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=0.8, random_state= 25)
@@ -23,10 +35,12 @@ lab_enc = preprocessing.LabelEncoder()
 y_train = lab_enc.fit_transform(y_train)
 print(utils.multiclass.type_of_target(y_train))
 
-
+############################################################
+############  MODELO
+############
+############################################################
 clf = tree.DecisionTreeClassifier(max_depth= 5)
 clf = clf.fit(X_train, y_train)
-
 
 tree.plot_tree(clf)
 plt.show()

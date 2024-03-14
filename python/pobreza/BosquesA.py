0cd14a59b891f52da47068931cc4323ee3f97419
antonioespinoza98 <espinozamarco70@gmail.com>
2024-03-14 11:09:31 -0300

    ma;ana

---
+++ python/pobreza/BosquesA.py
@@ -0,0 +1,54 @@
+############################################################
+############  LIBRERIAS
+############
+############################################################
+from sklearn.model_selection import train_test_split
+from sklearn import preprocessing, utils, tree
+import pyreadr
+import pandas as pd
+import graphviz
+import matplotlib.pyplot as plt
+
+############################################################
+############  BASE DE DATOS
+############
+############################################################
+# BASE DE DATOS
+## Como la base está en un archivo RData, se utiliza el paquete de pyreadr para poder leerlo en Python.
+
+result = pyreadr.read_r("./data/ingreso/encuesta_df_agg.Rdata")
+print(result.keys())
+base = result["encuesta_df_agg"]
+## Ahora se crea la base de datos de entrenamiento y de prueba
+## Revisamos que la base de datos esté completa
+base.head()
+list(base.columns)
+
+############################################################
+############  ENTRENAMIENTO/PRUEBA
+############
+############################################################
+X = base.drop(base.columns[6:9], axis=1)
+y = base.iloc[:, 5]
+list(X.columns)
+X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=0.8, random_state= 25)
+lab_enc = preprocessing.LabelEncoder()
+y_train = lab_enc.fit_transform(y_train)
+print(utils.multiclass.type_of_target(y_train))
+
+############################################################
+############  MODELO
+############
+############################################################
+clf = tree.DecisionTreeClassifier()
+clf = clf.fit(X_train, y_train)
+
+tree.plot_tree(clf, feature_names= list(X.columns))
+plt.show()
+
+dot_data = tree.export_graphviz(clf, out_file=None,
+                                filled=True, rounded=True,  
+                                special_characters=True)  
+graph = graphviz.Source(dot_data)  
+graph
+plt.show()
